0088acb9ccf5459d217a974eb8235b05792785de
Joshua Forden <26302898+air4den@users.noreply.github.com>
2024-02-11 02:57:09 -0500

    Connected model to frontend

--- clickbuzz/main.py
+++ clickbuzz/main.py
@@ -3,16 +3,92 @@ from taipy.core import Core
 from taipy.config import Config
 import taipy as tp
 
+import joblib
+import xgboost
+
 scenario = None
 
 def on_submit(state):
-    state.scenario.input_titles.write([state.title1, state.title2, state.title3, state.title4, state.title5])
+    state.scenario.input_titles.write([state.title1, state.title2])
     state.scenario.submit()
-    state.best_title_text = scenario.message.read()
+    titles = [state.title1, state.title2]
 
-def get_best_title(titles):
-    # TODO: Change to actual algorithm
-    return f"The best one: {titles[0]}"
+    state.grade1 = get_grade(get_score(state.title1))
+    state.grade2 = get_grade(get_score(state.title2))
+    
+    bestTitle = get_best_title(titles[0], titles[1])
+    
+    state.best_title_text = bestTitle
+    
+
+def get_score(title):
+    loaded_model= joblib.load('model.joblib')
+    score = min((loaded_model.predict([title])[0]/3 + 0.666), 1.0)
+    print(score)
+    return score
+
+def get_grade(outScore):
+    outScore *= 100
+    outGrade = ""
+    if outScore >= 90:
+        outGrade = "A"
+    elif outScore >= 80:
+        outGrade = "B"
+    elif outScore >= 70:
+        outGrade = "C"
+    elif outScore >= 60:
+        outGrade = "D"
+    else:
+        outGrade = "F"
+    
+    # Determine if it's a + or -
+    if outScore % 10 >= 7 and outGrade != "F":
+        outGrade += "+"
+    elif outScore % 10 <= 3 and outGrade != "F":
+        outGrade += "-"
+
+    return outGrade
+
+def compare_grades(grades):
+    grade_values = {
+        "A+": 13, "A": 12, "A-": 11, 
+        "B+": 10, "B": 9, "B-": 8, 
+        "C+": 7, "C": 6, "C-": 5, 
+        "D+": 4, "D": 3, "D-": 2,
+        "F": 1
+    }   
+    highest_index = 0
+    
+    if grade_values[grades[0]] > grade_values[grades[1]]:
+        highest_index = 0
+    elif grade_values[grades[0]] < grade_values[grades[1]]:
+        highest_index = 1
+    else:
+        return -1
+    return highest_index
+
+
+def get_best_title(title0, title1):
+    loaded_model= joblib.load('model.joblib')
+    score0 = get_score(title0)
+    score1 = get_score(title1)
+
+    # Assign Letter Grade
+    grade1 = get_grade(score0)
+    grade2 = get_grade(score1)
+    grades = [grade1, grade2]
+
+    betterTits = compare_grades(grades)
+
+    outTitle = ''
+    if betterTits == 0:
+        outTitle = title0
+    elif betterTits == 1: 
+        outTitle = title1
+    else:
+        outTitle = "You choose! Both titles scored equally well. "
+
+    return f"The best one: {outTitle}"
 
 titles_cfg = Config.configure_data_node(id="input_titles")
 message_data_node_cfg = Config.configure_data_node(id="message")
@@ -22,6 +98,8 @@ scenario_cfg = Config.configure_scenario("scenario", task_configs=[get_best_cfg]
 title1 = ""
 title2 = ""
 
+grade1 = ''
+grade2 = ''
 
 best_title_text = "Need to submit"
 
@@ -29,8 +107,8 @@ manual_md = """
 # Welcome to ClickBUZZ!
 
 ## Input Your A|B Titles \n
-A  <|{title1}|input|> \n 
-B  <|{title2}|input|> \n
+A  <|{title1}|input|> Grade: <|{grade1}|text|> \n 
+B  <|{title2}|input|> Grade: <|{grade2}|text|> \n
 
 
 <|Submit|button|on_action=on_submit|id=button_submit|> \n
